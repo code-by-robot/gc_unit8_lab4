d563cc05879487d0784182f78625dd3886337496
robot <rahaupt0@gmail.com>
2022-08-03 16:01:19 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
-
+import { MenuItem } from './menu';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LectureAngular';
+  title = 'Sit down and have something to eat!';
+  menu: Array<MenuItem> = [{
+    Item: 'Chimken Fingies',
+    Category: 'dinner',
+    Price: 11.99
+  },
+  {
+    Item: 'Pizza',
+    Category: 'dinner',
+    Price: 11.99
+  },
+  {
+    Item:'Wings',
+    Category:'sides',
+    Price: 8.99
+  },
+  {
+    Item: 'Breadsticks',
+    Category: 'sides',
+    Price: 4.99
+  },
+  {
+    Item: 'Ceasar Salad',
+    Category: 'salads',
+    Price: 5.99
+  },
+  {
+    Item:'Cinnamon Roll',
+    Category:'dessert', 
+    Price:8.99
+  }]
 }
+
+
